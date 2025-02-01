63aab1a16141316414f330c21d2ac076515da6a8
silverbaum <39158562+TaxiDriver202@users.noreply.github.com>
2025-02-01 14:00:19 +0200

    update to dynamic tags

--- app/app.py
+++ app/app.py
@@ -1,88 +1,127 @@
 from gevent import monkey
 monkey.patch_all()
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_socketio import SocketIO, emit
-import os
-from tags import * #MAC addresses, soon unnecessary
+from os import getenv
 
+"""
+from os import path
+import sqlite3
+
+
+dbpath = path.join("app", "data", "ruuvidata.db")
+cx = sqlite3.connect(dbpath)
+cursor = cx.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS ruuvi(Temperature float, Humidity float, Pressure float, Date date)')
+"""
 
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins='*', async_mode='gevent')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+objs = [] # list which contains the ruuvitag objects
 
-# Dictionaries to hold the Ruuvi data variables
-# WIP: create dynamic data storage to enable other/more tags
-magicmountain = {
-    "temp": 0,
-    "humidity": 0,
-    "pressure": 0,
-    "acceleration_x": 0,
-    "acceleration_y": 0,
-    "acceleration_z": 0
-}
 
-lodge = {
-    "temp": 0,
-    "humidity": 0,
-    "pressure": 0,
-    "acceleration_x": 0,
-    "acceleration_y": 0,
-    "acceleration_z": 0
-}
+#RuuviTag object class
+class RuuviTag:
+ def __init__(self):
+      self.data = {
+	  "temperature": 0,
+	  "humidity": 0,
+	  "pressure": 0
+	  }
+ def updata(self, data):
+      self.data.update({
+	  "temperature": data.get('temperature', 0),
+	  "humidity": data.get('humidity', 0),
+	  "pressure": (data.get('pressure', 0)/1000)}
+	  )
+      
 
+
+def objectifier(ntag): #function to create the RuuviTag objects
+    global objs # I wish it wasn't global, currently required by both dashboard and request_data functions
+    if ntag == 0:
+        objs.clear()
+        objs = []
+
+    for i in range(ntag):
+        i = RuuviTag()
+        objs.append(i)
+    return objs
+
+
+
+#redirect to dash
 @app.route('/')
+def redirecter():
+    return  redirect(url_for('dashboard'), code=308)
+
+#dashboard
+@app.route('/dashboard')
 def dashboard():
-    data = {
-        'magicmountain': magicmountain,
-        'lodge': lodge
-    }
+    data = {}
+    for i, obj in enumerate(objs): # creates indexed list of objs (0, obj[0]), (1, obj[1])
+        data[f"Tag {i}"] = obj.data 
+    
     return render_template('dashboard.html', data=data)
 
-  
+#data route
 @app.route('/request', methods=['POST'])
-def request_data(): 
-    global magicmountain, lodge
+def request_data():
     try:
+        """Receives the JSON data, finds"""
+        #assigning variables
+        cleandata = {}
         data = request.get_json()
         tags = data['data']['tags']
-        # Define tags with MAC addresses
-        tag_magicmountain = tags[magicmountainmac]
-        tag_lodge = tags[lodgemac]
-        #determine order of tags
-        # Update magicmountain data
-        magicmountain.update({
-            "temp": tag_magicmountain.get('temperature', 0),
-            "humidity": tag_magicmountain.get('humidity', 0),
-            "pressure": (tag_magicmountain.get('pressure', 0)/1000),
-            "acceleration_x": tag_magicmountain.get('acceleration_x', 0),
-            "acceleration_y": tag_magicmountain.get('acceleration_y', 0),
-            "acceleration_z": tag_magicmountain.get('acceleration_z', 0)
-        })
+        num_of_tags = len(tags) 
+        tag_macs = tags.keys() #keys from the tags dictionary
+
+
+        #creating the ruuvitag objects
+        if num_of_tags != len(objs): #clear the objects if the amount of tags changes
+            objectifier(0)
+        if objs == []: #if the list is empty i.e. no objects, create new objects
+            objectifier(num_of_tags) # create tag objects
+
         
-        # Update lodge data
-        lodge.update({
-            "temp": tag_lodge.get('temperature', 0),
-            "humidity": tag_lodge.get('humidity', 0),
-            "pressure": (tag_lodge.get('pressure', 0)/1000),
-            "acceleration_x": tag_lodge.get('acceleration_x', 0),
-            "acceleration_y": tag_lodge.get('acceleration_y', 0),
-            "acceleration_z": tag_lodge.get('acceleration_z', 0)
-        })
 
-        #print(f"Magic Mountain: {magicmountain}, Lodge: {lodge}")
-        #print(tags) # print "raw" data to terminal for debugging
-        socketio.emit('data_update', {'magicmountain': magicmountain, 'lodge': lodge})  
+        
+        #iterate through the data using the mac addresses and place it in the cleandata dict
+        datanameiterator = 0
+        for tag in tag_macs:
+            cleandata[datanameiterator] = data['data']['tags'][tag]['data']
+            datanameiterator += 1
+        
+        
+        #update the generated objects with the received data
+        for i in range(num_of_tags):
+        
+            objs[i].updata(cleandata[i])
 
-        return jsonify({"status": "success", "data": data}), 200
+        # Create packets with tag names
+        packets = {}
+        for i in range(len(objs)):
+            packets[f"Tag {i}"] = objs[i].data # name packets
+        
+        print("Emitting data update:", packets)  # Debug print
+        socketio.emit('data_update', packets)
+
+        return jsonify({"status": "success", "data": packets}), 200
     except Exception as e:
+        print("Error in request_data:", str(e))  # Debug print
         return jsonify({"status": "error", "message": str(e)}), 400
 
+
+#command for gunicorn:
+#gunicorn --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker --workers 1 --bind 0.0.0.0:5000 app.app:app
+
 if __name__ == '__main__':
-    if os.getenv('FLASK_ENV') == 'development':
+    if getenv('FLASK_ENV') == 'development':
         socketio.run(app, debug=True, host='0.0.0.0', port=5000)
     else:
         from gunicorn.app.base import BaseApplication
-        from six import iteritems
+    
 
         class StandaloneApplication(BaseApplication):
             def __init__(self, app, options=None):
@@ -91,16 +130,16 @@ if __name__ == '__main__':
                 super(StandaloneApplication, self).__init__()
 
             def load_config(self):
-                config = {key: value for key, value in iteritems(self.options)
+                config = {key: value for key, value in self.options.items()
                           if key in self.cfg.settings and value is not None}
-                for key, value in iteritems(config):
+                for key, value in config.items():
                     self.cfg.set(key.lower(), value)
 
             def load(self):
                 return self.application
 
         options = {
-            'bind': '0.0.0.0:5000',
+            'bind': '0.0.0.0:5002',
             'workers': 1,
             'worker_class': 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
 
