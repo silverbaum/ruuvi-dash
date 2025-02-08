839b21694cb93db604439d0a7c63245c55f51bb4
silverbaum <39158562+TaxiDriver202@users.noreply.github.com>
2025-02-08 17:11:04 +0200

    docs, removed socketIO getenv & unnecessary imports, shortened license text.

--- app/app.py
+++ app/app.py
@@ -1,30 +1,10 @@
-"""
-Copyright (c) 2025 Topias Silfverhuth
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+#SPDX-FileCopyrightText: 2025 Topias Silfverhuth
+#SPDX-License-Identifier: MIT
 
 from gevent import monkey
 monkey.patch_all()
 from flask import Flask, request, jsonify, render_template, redirect, url_for
-from flask_socketio import SocketIO, emit
-from os import getenv
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
@@ -62,8 +42,8 @@ def dashboard():
 def request_data():
     """Receives gateway data in json and sends processed data to clients with SocketIO
     
-    Currently only supports decoded data. (data format 5 in gw json)
-    The data is 'saved' in an instance of the Tags class.
+    Currently only supports decoded data.
+    The data is then contained within an instance of the Tags class.
     
     """
     try:
@@ -87,10 +67,7 @@ def request_data():
 
 
 if __name__ == '__main__':
-    if getenv('FLASK_ENV' == 'development'):
         socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-    else:
-        socketio.run(app, debug=False, host='0.0.0.0', port=5000)
         
 """
 command for gunicorn(for docker place in dockerfile CMD[]):
